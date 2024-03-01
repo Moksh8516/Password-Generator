c78f856c15246ca8ff64e8037f5de238ab36c3d2
Moksh <moksh5340@gmail.com>
2024-03-02 01:09:32 +0530

    add url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+URL: 'https://moksh8516.github.io/Password-Generator/subscribe'
 export default defineConfig({
   plugins: [react()],
 })
